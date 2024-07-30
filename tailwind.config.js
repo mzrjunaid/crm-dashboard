9c0a0247cb441df4840ed22d595e32b6724bd559
mzrjunaid <mzrjunaid@gmail.com>
2024-07-30 15:44:02 +0500

    CRM Dashboard

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#5932EA",
+        accent: "#9197B3",
+      },
+    },
+  },
+  plugins: [],
+};
