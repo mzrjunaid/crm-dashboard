fe320137679e8072376597e6dd8bf29a4bbe53fe
Junaid <mzrjunaid@gmail.com>
2024-08-02 10:19:11 +0500

    updated and upgraded

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ module.exports = {
     extend: {
       colors: {
         primary: "#5932EA",
+        secondary: "#00AC4F",
         accent: "#9197B3",
+        shadowColor: "#E2ECF9",
+        error: "#D0004B",
+        black: "#333333",
+        background: "#FAFBFF",
       },
     },
   },
